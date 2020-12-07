f0d8dc46916c67f8568dbdcad5804f622bb75e04
yuka-lamp <oonishi@lamp.jp>
2020-12-07 16:13:11 +0900

    update

--- page-service.php
+++ page-service.php
@@ -94,14 +94,14 @@ get_header(); ?>
 <div class="lp__feature__list__inner-wrap">
 <h3>京都を中心に<br><span>どこでも</span>お届けいたします！</h3>
 <img src="<?php echo $img_url; ?>lp_feature_1.png" alt="京都を中心にどこでもお届けいたします！" srcset="<?php echo $img_url; ?>lp_feature_1.png 1x, <?php echo $img_url; ?>lp_feature_1@2x.png 2x">
-<p>初期導入費・月額利用料は全て無料！店舗が負担するのは注文時のサービス手数料のみで、固定費はかかりません。（※オプションをご選択の場合は別途費用）</p>
+<p>テイクアウトやデリバリーに注力されている飲食店の配送をお手伝いするべく、飲食店向けのサービスとして、タクシーで料理の配達を行う「MKのタク配」を実施し、京都市内を中心に商品をお客様のもとへお届けいたします。</p>
 </div>
 </div>
 <div class="lp__feature__list__inner">
 <div class="lp__feature__list__inner-wrap">
 <h3>MKタクシーが<br><span>大切</span>お届けします！</h3>
 <img src="<?php echo $img_url; ?>lp_feature_2.png" alt="京都を中心にどこでもお届けいたします！" srcset="<?php echo $img_url; ?>lp_feature_2.png 1x, <?php echo $img_url; ?>lp_feature_2@2x.png 2x">
-<p>初期導入費・月額利用料は全て無料！店舗が負担するのは注文時のサービス手数料のみで、固定費はかかりません。（※オプションをご選択の場合は別途費用）</p>
+<p>MKタクシーが飲食店様へ商品を受け取りに向かいます。お預かりした商品を、専用BOXにてお客様のもとへ大切にお届けいたします。（容器や梱包はお客様でお願いいたします。）</p>
 </div>
 </div>
 <div class="lp__feature__list__inner">
@@ -115,7 +115,7 @@ get_header(); ?>
 <div class="lp__feature__list__inner-wrap">
 <h3>MKタク配のサイトで<br>あなたの<span>お店を紹介</span></h3>
 <img src="<?php echo $img_url; ?>lp_feature_4.png" alt="京都を中心にどこでもお届けいたします！" srcset="<?php echo $img_url; ?>lp_feature_4.png 1x, <?php echo $img_url; ?>lp_feature_4@2x.png 2x">
-<p>初期導入費・月額利用料は全て無料！店舗が負担するのは注文時のサービス手数料のみで、固定費はかかりません。（※オプションをご選択の場合は別途費用）</p>
+<p>MKタク配お店検索サイトにてあなたのお店が掲載されます。店舗サイト（TakeEats)を導入の方は、お客様のネット注文が簡単です。</p>
 </div>
 </div>
 </div>
@@ -130,18 +130,29 @@ get_header(); ?>
 </div>
 <p class="small my-3 text-md-center">※オプションをご選択の場合は別途費用がかかります。
 <br>※クレジットカードでの決済の場合手数料3.6%が別途発生します。</p>
+<div class="another-plan mt-5">
+  <div class="row">
+
+  </div>
+<h3 class="mb-3"><span class="text-primary">■</span> 宅配のみのご利用も可能です</h3>
+<ul>
+<li>宅配料1回550円（税込）</li>
+<li>配達以来はお電話で！（24時間対応）</li>
+<li>京都市外メーターも配達可能</li>
+</ul>
+<p class="m-0">※ 商品代の収受に関して、MKは一切関与いたしません。料理代金は直接お客様と精算願います。
+<br>※ 事前決済の場合：自社サイトや宅配サイトなど、オンライン決済が可能な飲食店様はそちらをご利用ください。
+<br>※ 事後決済の場合：事後決済はQRコード決済のみ可能です。飲食店様が契約されているQRコード決済のシールやQRコードが記された用紙をご用意いただければ、お届け時にお客様が送金操作をするところまでドライバーは確認いたします。
+<br>※ 決済完了、不能、金額差額などについては一切の責任を負いません。また、QRコードが記された用紙をお店に返却することはできませんのでコピーなどのご用意をお願いいたします。</p>
+</div>
+</div>
 </section>
 
 <section class="lp__flow bg-light">
 <div class="container">
 <h2 class="lp-ttl2"><span>導入の流れ</span></h2>
-<ul class="nav nav-tabs">
-<li class="active"><a href="#takeeats" data-toggle="tab">システムを導入する場合</a></li>
-<li><a href="#tel" data-toggle="tab">システムを導入しない場合</a></li>
-</ul>
+
 <div class="lp__flow__wrap">
-<div class="tab-content">
-<div class="tab-pane active" id="takeeats">
 <ol class="lp__flow__list">
 <li class="lp__flow__list__inner">
 <div class="lp__flow__list__inner-step">STEP<span>01</span></div>
@@ -168,50 +179,22 @@ get_header(); ?>
 </li>
 <!-- lp__flow__list__inner -->
 </ol>
-</div>
-<div class="tab-pane" id="tel">
-<ol class="lp__flow__list">
-<li class="lp__flow__list__inner">
-<div class="lp__flow__list__inner-step">STEP<span>01</span></div>
-<h3>注文の受付()</h3>
-<p>お申込みフォームより必要事項のご入力をお願い致します。<br>担当者よりお電話にてご連絡致します。</p>
-</li>
-<!-- lp__flow__list__inner -->
-<li class="lp__flow__list__inner">
-<div class="lp__flow__list__inner-step">STEP<span>02</span></div>
-<h3>アカウント発行</h3>
-<p>お店の専用サイトを作成しアカウントを発行致します。</p>
-</li>
-<!-- lp__flow__list__inner -->
-<li class="lp__flow__list__inner">
-<div class="lp__flow__list__inner-step">STEP<span>03</span></div>
-<h3>メニュー登録</h3>
-<p>販売する商品の登録を行います。<br>(※無料代行サービス実施中)</p>
-</li>
-<!-- lp__flow__list__inner -->
-<li class="lp__flow__list__inner">
-<div class="lp__flow__list__inner-step">STEP<span>04</span></div>
-<h3>ご利用スタート</h3>
-<p>準備が整ったら早速注文の受け付けを行いましょう。</p>
-</li>
-<!-- lp__flow__list__inner -->
-</ol>
-</div>
-</div>
 <!-- lp__flow__list -->
 </div>
+
+<p class="small text-center mt-4">※ 宅配のみをご利用の店舗様は、初回ご利用時商品と一緒にドライバーに申込書渡すだけで始められます。</p>
 </div>
 </section>
 <!-- flow -->
 
 <div id="toform" class="lp__cta mt-5 footer-ctabtn">
 <div class="lp__cta__box">
-<a class="smooth-scroll lp__cta__box_btn" href="<?php echo $home; ?>/seminar"><img class="mr-2" src="<?php echo $img_url; ?>cta_btn.svg" style="width:18px;
-  height:auto" alt="吹き出しアイコン">まずはお問い合わせ</a>
+<a class="smooth-scroll lp__cta__box_btn" href="#contact"><img class="mr-2" src="<?php echo $img_url; ?>cta_btn.svg" style="width:18px;
+height:auto" alt="吹き出しアイコン">まずはお問い合わせ</a>
 </div>
 </div>
 
-<section class="lp__form">
+<section id="contact" class="lp__form">
 <div class="container">
 <div class="text-center lp__form-top">
 <h2 class="lp-ttl2"><span class="marks">お問い合わせ</span></h2>
