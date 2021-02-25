a6b7ae718b670f65b9cbf3fb21839a3e211fb106
yuka-lamp <oonishi@amp>
2021-02-25 19:15:35 +0900

    update

--- archive-shop.php
+++ archive-shop.php
@@ -129,29 +129,13 @@ get_header(); ?>
       </div>
 
       <!-- ▼ ページネーション -->
-      <div class="pagination">
-          <?php global $wp_rewrite;
-          $paginate_base = get_pagenum_link(1);
-          if(strpos($paginate_base, '?') || ! $wp_rewrite->using_permalinks()){
-              $paginate_format = '';
-              $paginate_base = add_query_arg('paged','%#%');
-          }
-          else{
-              $paginate_format = (substr($paginate_base,-1,1) == '/' ? '' : '/') .
-              user_trailingslashit('page/%#%/','paged');;
-              $paginate_base .= '%_%';
-          }
-          echo paginate_links(array(
-              'base' => $paginate_base,
-              'format' => $paginate_format,
-              'total' => $wp_query->max_num_pages,
-              'mid_size' => 4,
-              'current' => ($paged ? $paged : 1),
-              'prev_text' => '<< 前へ',
-              'next_text' => '次へ >>',
-          )); ?>
-      </div>
+      <?php
+      if (function_exists('wp_pagenavi')) {
+      wp_pagenavi(['query' => $wp_query]);
+      }
+      ?>
       <!-- ▲ ページネーション -->
+
       <?php else: // ないとき ?>
       <p class="text-center pt-5 pb-1 d-block w-100">該当の店舗がありません。<br>条件を変更して検索してください。</p>
       <!-- ▼ 条件を変更 -->
